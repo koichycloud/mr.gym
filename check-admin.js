44824afa48c81b356e7075060d6f170dea3aad84
koichy <albertoarias55@gmail.com>
2026-02-10 22:46:23 -0500

    Chore: Cleanup debug scripts

--- check-admin.js
+++ check-admin.js
@@ -2,6 +2,8 @@
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
+const bcrypt = require('bcryptjs')
+
 async function main() {
     try {
         const user = await prisma.user.findUnique({
